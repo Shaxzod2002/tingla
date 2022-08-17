bd4b1adddf230da161c5545a381eb51cc097cba2
WebShohDev <raximovshaxzodbek2002@gmail.com>
2022-08-17 12:58:19 +0000

    latest version with bugs fixed

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let menuHour = document.querySelector('.date__hour');
 let menuMinute = document.querySelector('.date__minute');
 let menuSecond = document.querySelector('.date__second');
+let topScroll = document.querySelector('.top__scroll');
 
 let getDate = () => {
     (menuHour.innerText < 9) ? menuHour.innerHTML = '0' + new Date().getHours() : menuHour.innerHTML = new Date().getHours();
@@ -11,3 +12,20 @@ let getDate = () => {
 }
 setInterval(getDate, 1000);
 
+window.addEventListener('contextmenu', e => e.preventDefault())
+
+window.addEventListener('scroll', () => {
+    topScroll.classList.toggle('scroll', scrollY > 400);
+})
+
+window.document.onkeydown = function (e) {
+    if (e.ctrlKey &&
+        (e.keyCode === 67 ||
+            e.keyCode === 86 ||
+            e.keyCode === 85 ||
+            e.keyCode === 117)) {
+        return false;
+    } else {
+        return true;
+    }
+};
